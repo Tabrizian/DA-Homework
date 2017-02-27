f33ae2280bd992bfb6aaf3884897325f52e98d2c
Iman Tabrizian <Tabrizian@outlook.com>
2017-02-27 11:42:48 +0330

    Adds new source codes

--- src/comparator.vhd
+++ src/comparator.vhd
@@ -7,8 +7,7 @@ entity comparator is
             b: in std_logic_vector(1 downto 0);
             gt: out std_logic;
             eq: out std_logic;
-            lt: out std_logic;
-
+            lt: out std_logic
     );
 end entity;
 
@@ -16,7 +15,7 @@ architecture struct of comparator is
 begin
     gt <= ((a(1) xor b(1)) and a(1)) or ((a(0) xor b(0)) and a(0));
     eq <= ((a(1) xnor b(1)) and (a(0) xnor b(0)));
-    lt <= 0;
+    lt <= '0';
 end struct;
 
 architecture behavorial of comparator is
@@ -24,17 +23,17 @@ begin
     process(a, b)
     begin
         if(a > b) then
-            gt <= 1;
-            lt <= 0;
-            eq <= 0;
+            gt <= '1';
+            lt <= '0';
+            eq <= '0';
         elsif(a < b) then
-            lt <= 1;
-            gt <= 0;
-            eq <= 0;
+            lt <= '1';
+            gt <= '0';
+            eq <= '0';
         else
-            eq <=1;
-            gt <= 0;
-            lt <= 0;
+            eq <= '1';
+            gt <= '0';
+            lt <= '0';
         end if;
     end process;
 end behavorial;
---
+++ test/comparator_tb.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity comparator_tb is
+
+end entity;
+
+architecture tb of comparator_tb is
+    component comparator is
+        port(
+                a: in std_logic_vector(1 downto 0);
+                b: in std_logic_vector(1 downto 0);
+                gt: out std_logic;
+                eq: out std_logic;
+                lt: out std_logic
+        );
+    end component;
+
+    signal a, b: std_logic_vector(1 downto 0);
+    signal gt, eq, lt: std_logic;
+begin
+    mapping: comparator port map(a, b, gt, eq, lt);
+
+    a <= "11" after 0 ns,
+         "10" after 5 ns,
+         "01" after 10 ns,
+         "00" after 15 ns;
+
+    b <= "00" after 0 ns,
+         "01" after 5 ns,
+         "10" after 10 ns,
+         "11" after 15 ns;
+
+end tb;
