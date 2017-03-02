935f7047d179642506bd263c0a59ee04b6b48f72
Iman Tabrizian <Tabrizian@outlook.com>
2017-03-02 22:18:02 +0330

    Adds bcd mutliplier

--- project-phase-1/src/bcd_multiplier.vhd
+++ project-phase-1/src/bcd_multiplier.vhd
@@ -1,28 +1,15 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity full_adder is
+entity bcd_multiplier is
     port(
-            v0: in std_logic;
-            v1: in std_logic;
-            v2: in std_logic;
-            v3: in std_logic;
-            v4: in std_logic;
-            v5: in std_logic;
-            v6: in std_logic;
-            v7: in std_logic;
-            a0: out std_logic;
-            a1: out std_logic;
-            a2: out std_logic;
-            a3: out std_logic;
-            b0: out std_logic;
-            b1: out std_logic;
-            b2: out std_logic;
-            b3: out std_logic
+            a: in std_logic_vector(3 downto 0);
+            b: in std_logic_vector(3 downto 0);
+            v: out std_logic_vector(7 downto 0)
         );
 end entity;
 
-architecture behavorial of full_adder is
+architecture struct of bcd_multiplier is
     component half_adder
         port(
                 a: in std_logic;
@@ -32,5 +19,98 @@ architecture behavorial of full_adder is
             );
     end component;
 
+    component full_adder
+        port(
+                a: in std_logic;
+                b: in std_logic;
+                cin: in std_logic;
+                s: out std_logic;
+                cout: out std_logic
+            );
+    end component;
+
+    component multiplier_2_by_2
+        port(
+                a: in std_logic_vector(1 downto 0);
+                b: in std_logic_vector(1 downto 0);
+                res: out std_logic_vector(3 downto 0)
+            );
+    end component;
+    signal multiplier_2_by_2_1_res: std_logic_vector(3 downto 0);
+    signal multiplier_2_by_2_2_res: std_logic_vector(3 downto 0);
+    signal multiplier_2_by_2_3_res: std_logic_vector(3 downto 0);
+    signal multiplier_2_by_2_4_res: std_logic_vector(3 downto 0);
+
+    signal full_adder_1_s: std_logic;
+    signal full_adder_1_cout: std_logic;
+
+    signal full_adder_2_s: std_logic;
+    signal full_adder_2_cout: std_logic;
+
+    signal full_adder_3_s: std_logic;
+    signal full_adder_3_cout: std_logic;
+
+    signal full_adder_4_s: std_logic;
+    signal full_adder_4_cout: std_logic;
+
+    signal full_adder_5_s: std_logic;
+    signal full_adder_5_cout: std_logic;
+
+    signal full_adder_6_s: std_logic;
+    signal full_adder_6_cout: std_logic;
+
+    signal full_adder_7_s: std_logic;
+    signal full_adder_7_cout: std_logic;
+
+    signal half_adder_1_s: std_logic;
+    signal half_adder_1_cout: std_logic;
+
+    signal half_adder_2_s: std_logic;
+    signal half_adder_2_cout: std_logic;
 begin
-end behavorial;
+    multiplier_2_by_2_1: multiplier_2_by_2 port map(b(1 downto 0),
+                         a(1 downto 0), multiplier_2_by_2_1_res);
+
+    multiplier_2_by_2_2: multiplier_2_by_2 port map(b(1 downto 0),
+                         a(3 downto 2), multiplier_2_by_2_2_res);
+
+    multiplier_2_by_2_3: multiplier_2_by_2 port map(b(3 downto 2),
+                         a(1 downto 0), multiplier_2_by_2_3_res);
+
+    multiplier_2_by_2_4: multiplier_2_by_2 port map(b(3 downto 2),
+                         a(3 downto 2), multiplier_2_by_2_4_res);
+
+    v(1 downto 0) <= multiplier_2_by_2_1_res(1 downto 0);
+
+    full_adder_1: full_adder port map(multiplier_2_by_2_1_res(2),
+                  multiplier_2_by_2_2_res(0), multiplier_2_by_2_3_res(0),
+                  full_adder_1_s, full_adder_1_cout);
+
+    v(2) <= full_adder_1_s;
+
+    full_adder_2: full_adder port map(multiplier_2_by_2_1_res(3),
+                  multiplier_2_by_2_2_res(1), multiplier_2_by_2_3_res(1),
+                  full_adder_2_s, full_adder_2_cout);
+
+    full_adder_3: full_adder port map(full_adder_2_cout,
+                  multiplier_2_by_2_2_res(2), multiplier_2_by_2_3_res(2),
+                  full_adder_3_s, full_adder_3_cout);
+
+    half_adder_1: half_adder port map(full_adder_1_cout, full_adder_2_s, v(3),
+                  half_adder_1_cout);
+
+    full_adder_6: full_adder port map(half_adder_1_cout, full_adder_3_s,
+                  multiplier_2_by_2_2_res(3), v(4), full_adder_6_cout);
+
+    full_adder_5: full_adder port map(multiplier_2_by_2_4_res(0), full_adder_3_cout,
+                  multiplier_2_by_2_3_res(3), full_adder_5_s, full_adder_5_cout);
+
+    full_adder_7: full_adder port map(full_adder_6_cout, full_adder_5_s,
+                  multiplier_2_by_2_4_res(1), v(5), full_adder_7_cout);
+
+    full_adder_4: full_adder port map(full_adder_7_cout, full_adder_5_cout,
+                  multiplier_2_by_2_4_res(2), v(6), full_adder_4_cout);
+
+    half_adder_2: half_adder port map(full_adder_4_cout, multiplier_2_by_2_4_res(3),
+                  v(7), half_adder_2_cout);
+end struct;
---
+++ project-phase-1/test/bcd_multiplier_tb.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity bcd_multiplier_tb is
+end entity;
+
+architecture tb of bcd_multiplier_tb is
+    component bcd_multiplier is
+        port(
+                a: in std_logic_vector(3 downto 0);
+                b: in std_logic_vector(3 downto 0);
+                v: out std_logic_vector(7 downto 0)
+            );
+    end component;
+
+    signal a: std_logic_vector(3 downto 0);
+    signal b: std_logic_vector(3 downto 0);
+    signal v: std_logic_vector(7 downto 0);
+
+begin
+    mapping: bcd_multiplier port map(a, b, v);
+
+    a <= "0000" after 0 ns,
+         "0100" after 10 ns,
+         "1000" after 20 ns,
+         "1100" after 30 ns,
+         "0000" after 40 ns,
+         "0100" after 50 ns,
+         "1000" after 60 ns,
+         "1100" after 70 ns,
+         "0000" after 80 ns,
+         "0100" after 90 ns,
+         "1000" after 100 ns,
+         "1100" after 110 ns;
+
+    b <= "0000" after 0 ns,
+         "0000" after 10 ns,
+         "0000" after 20 ns,
+         "0000" after 30 ns,
+         "0100" after 40 ns,
+         "0100" after 50 ns,
+         "0100" after 60 ns,
+         "0100" after 70 ns,
+         "1100" after 80 ns,
+         "1100" after 90 ns,
+         "1100" after 100 ns,
+         "1100" after 110 ns;
+
+end tb;
